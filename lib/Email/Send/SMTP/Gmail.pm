da835c27d54cef48e31664d869a80d0d76dc9970
Juan José San Martín <peco@cpan.org>
2014-04-15 08:23:12 +0000

    Import of PECO/Email-Send-SMTP-Gmail-0.83 from CPAN.
    gitpan-cpan-distribution: Email-Send-SMTP-Gmail
    gitpan-cpan-version:      0.83
    gitpan-cpan-path:         PECO/Email-Send-SMTP-Gmail-0.83.tar.gz
    gitpan-cpan-author:       PECO
    gitpan-cpan-maturity:     released

--- lib/Email/Send/SMTP/Gmail.pm
+++ lib/Email/Send/SMTP/Gmail.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use vars qw($VERSION);
 
-$VERSION='0.82';
+$VERSION='0.83';
 
 require Net::SMTPS;
 require Net::SMTP;
@@ -257,6 +257,7 @@ sub send
       $self->{sender}->datasend("Cc: " . $mail->{cc} . "\n") if ($mail->{cc} ne '');
       $self->{sender}->datasend("Reply-To: " . $mail->{replyto} . "\n");
       $self->{sender}->datasend("Subject: " . $mail->{subject} . "\n");
+      $self->{sender}->datasend("Date: " . localtime . "\n");
 
       if($mail->{attachments} ne '')
       {
